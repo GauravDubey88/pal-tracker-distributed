6bf240ef81ab0043829ab1ad13aff70e59710506
student <RakshaIsrani@dell.com>
2020-08-13 10:01:36 +0100

    eureka lab redo

--- platform-services/eureka-server/src/main/java/io/pivotal/pal/tracker/eurekaserver/App.java
+++ platform-services/eureka-server/src/main/java/io/pivotal/pal/tracker/eurekaserver/App.java
@@ -9,9 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class App {
 
     public static void main(String[] args) {
-
         SpringApplication.run(App.class, args);
-
     }
-
 }
